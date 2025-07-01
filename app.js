edc3dc041d59840e9d76fad9d018769f18fed1d8
MD-IESA-ALI-AHMED <mdiesa.24@kgpian.iitkgp.ac.in>
2025-07-01 17:08:39 +0530

    Signup page created

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path=require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,'public')))
 app.set("view engine","ejs")
 
 app.get("/",(req,res)=>{
